fc533d9ba6148822142d9d133415f1ac37d6df61
Identity Developer <DL-ID-Europa@sky.uk>
2016-09-29 15:25:57 +0100

    removed proxy logging

--- app/src/main/scala/com.example.proxy/ProxyProtocol.scala
+++
@@ -1,8 +0,0 @@
-package com.example.proxy
-
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import spray.json._
-
-trait ProxyProtocol extends DefaultJsonProtocol with SprayJsonSupport {
-  implicit val statusFormat: RootJsonFormat[Map[String, String]] = mapFormat[String, String]
-}
